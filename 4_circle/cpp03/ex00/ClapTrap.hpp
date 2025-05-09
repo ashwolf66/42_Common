5c4dcade6b8158dff4e24f1d3d158a19b2ced09d
ashwolf66 <wogur7890@gmail.com>
2025-05-09 15:36:39 +0900

    update

---
+++ 4_circle/cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jacha <jacha@student.42gyeongsan.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/09 15:27:26 by jacha             #+#    #+#             */
+/*   Updated: 2025/05/09 15:27:28 by jacha            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef CLAPTRAP_HPP
+#  define CLAPTRAP_HPP
+
+# include <iostream>
+
+class ClapTrap
+{
+private:
+	std::string _name;
+	int _HitPoint;
+	int _EnergyPoint;
+	int _AttackDamage;
+public:
+	ClapTrap();
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap& other);
+	ClapTrap& operator=(const ClapTrap& other);
+	~ClapTrap();
+	void	attack(const std::string& target);
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
+};
+
+# endif
---
+++ 4_circle/cpp03/ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jacha <jacha@student.42gyeongsan.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/09 15:27:30 by jacha             #+#    #+#             */
+/*   Updated: 2025/05/09 15:27:32 by jacha            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int 	main(void)
+{
+	ClapTrap human;
+	ClapTrap monster("vem");
+
+	human.attack("Dummy");
+	human.takeDamage(5);
+	human.beRepaired(10);
+	human.takeDamage(15);
+	human.beRepaired(10);
+	for (int i = 0; i < 11; i++)
+		monster.attack("Dummy");
+	return (0);
+}
