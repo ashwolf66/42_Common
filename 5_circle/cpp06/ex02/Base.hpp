cd057679b984c1557e4ad34c489903324f576a6d
Jaehyeok Cha <jacha@c3r20s3.42gyeongsan.kr>
2025-05-23 15:03:23 +0900

    update

--- 5_circle/cpp06/ex02/Base.hpp
+++ 5_circle/cpp06/ex02/Base.hpp
@@ -2,6 +2,8 @@
 #define BASE_HPP
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 class Base
 {
@@ -9,5 +11,9 @@ private:
 public:
 	virtual ~Base();
 };
+
+Base *generate(void);
+void identify(Base *p);
+void identify(Base &p);
 
 #endif
