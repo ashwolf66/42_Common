03931043095088d05110e00d52a7a877d68463b2
ashwolf66 <wogur7890@gmail.com>
2025-05-24 12:13:02 +0900

    cpp07 ex02

---
+++ 5_circle/cpp07/ex02/Array.hpp
@@ -0,0 +1,29 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+#include <cstdlib>
+
+template <typename T>
+class Array
+{
+	private:
+	T *_arr;
+	unsigned int _size;
+	
+	public:
+	Array();
+	Array(unsigned int n);
+	Array(const Array &other);
+	Array &operator=(const Array &other);
+	~Array();
+	
+	T &operator[](unsigned int i);
+	const T &operator[](unsigned int i) const;
+	
+	unsigned int size() const;
+};
+
+#include "Array.tpp"
+
+#endif
