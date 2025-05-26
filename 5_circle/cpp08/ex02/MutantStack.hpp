c471a60a16dbbcd65f1575bc3908167f93de84c2
ashwolf66 <wogur7890@gmail.com>
2025-05-26 18:08:02 +0900

    update

--- 5_circle/cpp08/ex02/MutantStack.hpp
+++ 5_circle/cpp08/ex02/MutantStack.hpp
@@ -27,19 +27,18 @@ public:
 	~MutantStack();
 
 	typedef typename std::stack<T>::container_type::iterator iterator;
-	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
-	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
-	typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
-
 	iterator begin();
 	iterator end();
 
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 	const_iterator begin() const;
 	const_iterator end() const;
 
+	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
 	reverse_iterator rbegin();
 	reverse_iterator rend();
 
+	typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 	const_reverse_iterator rbegin() const;
 	const_reverse_iterator rend() const;
 };
