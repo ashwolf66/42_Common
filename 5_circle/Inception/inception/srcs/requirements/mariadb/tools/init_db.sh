05d5776b22108c83626ffdfda2fe6b51c8bafc3c
ashwolf66 <wogur7890@gmail.com>
2025-08-16 16:03:54 +0900

    caos

--- 5_circle/Inception/inception/srcs/requirements/mariadb/tools/init_db.sh
+++ 5_circle/Inception/inception/srcs/requirements/mariadb/tools/init_db.sh
@@ -1,9 +1,25 @@
-
 #!/bin/bash
 
-service mysql start
+mkdir -p /run/mysqld
+chown -R mysql:mysql /var/lib/mysql /run/mysqld
 
-mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-mysql -e "FLUSH PRIVILEGES;"
+if [ ! -d /var/lib/mysql/mysql ]; then
+    echo "Initializing database..."
+    mysql_install_db --user=mysql > /dev/null
+fi
+
+mysqld_safe --skip-networking &
+sleep 5
+
+echo "Creating database and user..."
+mysql -u root <<EOF
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+killall mysqld
+sleep 3
+
+exec mysqld
--- 5_circle/Inception/inception/srcs/requirements/wordpress/tools/setup_wp.sh
+++ 5_circle/Inception/inception/srcs/requirements/wordpress/tools/setup_wp.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
+set -e
 
 cd /var/www/wordpress
 
-if [ ! -f wp-config.php ]; then
-	wget https://wordpress.org/latest.tar.gz
-	tar -xzf latest.tar.gz --strip-components=1
-	rm latest.tar.gz
+if [ ! -f index.php ]; then
+    wget https://wordpress.org/latest.tar.gz
+    tar -xzf latest.tar.gz --strip-components=1
+    rm latest.tar.gz
 fi
 
 cp wp-config-sample.php wp-config.php
-sed -i "s/database_name_here/${MYSQL_DATABASE}/" wp-config.php
-sed -i "s/username_here/${MYSQL_USER}/" wp-config.php
-sed -i "s/password_here/${MYSQL_PASSWORD}/" wp-config.php
-sed -i "s/localhost/mariadb/" wp-config.php
 
-exec "$@"
+sed -i "s#database_name_here#${MYSQL_DATABASE}#" wp-config.php
+sed -i "s#username_here#${MYSQL_USER}#" wp-config.php
+sed -i "s#password_here#${MYSQL_PASSWORD}#" wp-config.php
+sed -i "s#localhost#mariadb#" wp-config.php
+
+exec php-fpm -F
