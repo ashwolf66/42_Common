531b3c34345b2e5682793de74ff1304762a95a81
jacha <jacha@jacha.jacha.42.fr>
2025-08-20 11:28:14 +0900

    update: :

--- 5_circle/Inception/inception/srcs/requirements/mariadb/tools/init_db.sh
+++ 5_circle/Inception/inception/srcs/requirements/mariadb/tools/init_db.sh
@@ -6,20 +6,21 @@ chown -R mysql:mysql /var/lib/mysql /run/mysqld
 if [ ! -d /var/lib/mysql/mysql ]; then
     echo "Initializing database..."
     mysql_install_db --user=mysql > /dev/null
-fi
 
-mysqld_safe --skip-networking &
-sleep 5
+    echo "Creating database and user..."
+    mysqld --skip-networking --socket=/tmp/mysql.sock &
+    pid="$!"
+    sleep 5
 
-echo "Creating database and user..."
-mysql -u root <<EOF
+    mysql -u root --socket=/tmp/mysql.sock <<EOF
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-killall mysqld
-sleep 3
+    kill "$pid"
+    sleep 3
+fi
 
 exec mysqld
